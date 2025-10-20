c0aaa36f14938ad5b0403bfac0805ccff7478807
Jhowsoares <ensj2017@gmail.com>
2025-10-20 16:53:17 -0300

    Update app

--- testes.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-data_agr = datetime.now()
-
-hoje     = datetime.strptime(f'{data_agr.year}-{data_agr.month}-{data_agr.day}', '%Y-%m-%d')
-
-print(data_agr)
-print(hoje)
